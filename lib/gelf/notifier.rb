c743f0f2c17dca862bd59469f9148dc81c899bb3
Christian Rolle <christian.rolle@evopark.de>
2021-11-11 22:02:10 +0100

    Introduce HTTPS transport
    UDP and TCP are great if the server is within the same secured
    network.
    TCP with TLS is working if one wants to deal with certificates
    at the Graylog server.
    HTTPS should be introduced as an option to send logs via the
    internet in a encrypted way without necessarily setting up
    TCP-TLS.

--- lib/gelf/notifier.rb
+++ lib/gelf/notifier.rb
@@ -135,6 +135,8 @@ module GELF
   private
 
     def create_sender(host, port)
+      return create_https(host, port) if default_options['protocol'] == GELF::Protocol::HTTPS
+
       addresses = [[host, port]]
       if default_options['protocol'] == GELF::Protocol::TCP
         if default_options.key?('tls')
@@ -146,6 +148,10 @@ module GELF
       else
         GELF::Transport::UDP.new(addresses)
       end
+    end
+
+    def create_https(host, port)
+      GELF::Transport::HTTPS.new host, port: port, path: default_options.delete('path')
     end
 
     def notify_with_level(message_level, *args)
@@ -161,6 +167,8 @@ module GELF
       hash = extract_hash(*args)
       hash['level'] = message_level unless message_level.nil?
       if hash['level'] >= level
+        return @sender.transfer(hash) if default_options['protocol'] == GELF::Protocol::HTTPS
+
         if default_options['protocol'] == GELF::Protocol::TCP
           validate_hash(hash)
           @sender.send(hash.to_json + "\0")
